81eef08dc65f43525cf484e1cdae818794ea37e6
tundish <tundish@gigeconomy.org.uk>
2023-11-03 20:39:36 +0000

    Rethink tree structure.

--- rotu/test/test_conversation.py
+++ rotu/test/test_conversation.py
@@ -57,7 +57,9 @@ class Conversation(Drama):
         for key in path:
             try:
                 node = node[key]
+                print(f"node: {node}")
             except KeyError:
+                print("...bad.")
                 return
         else:
             return node
@@ -112,7 +114,7 @@ class Conversation(Drama):
         shots = self.follow_path(self.tree.tables, self.tree.shot_path) or []
 
         for shot in shots:
-            #print(f"shot: {shot}")
+            print(f"shot: {shot}")
             conditions = dict(director.specify_conditions(shot))
             if director.allows(conditions, self.tree.roles):
                 text = shot.get(director.dialogue_key, "")
@@ -157,34 +159,34 @@ class ConversationTests(unittest.TestCase):
     <ALAN> I'll let you carry on for a bit.
     '''
 
-    [[_.1._]]
+    [[_.1]]
     s='''
     <BETH> Well, you never know what's it's going to do next, do you?
     '''
 
-    [[_.1._]]
+    [[_.1]]
     s='''
     <BETH.returning> I've never seen anything like it!
     '''
 
-    [[_.2._]]
+    [[_.2]]
     s='''
     <BETH.branching> I've got two lovely cats.
         1. Ask about Charlie
         2. Ask about Doodles
     '''
 
-    [[_.2._.1._]]
+    [[_.2.1]]
     s='''
     <BETH> Charlie is the elder cat. He's a Marmalade. Very laid back.
     '''
 
-    [[_.3._]]
+    [[_.3]]
     s='''
     <BETH> I don't know anything about football at all.
     '''
 
-    [[_.2._.2._]]
+    [[_.2.2]]
     s='''
     <BETH> Oh my goodness, Doodles. Always up to mischief!
     '''
@@ -200,6 +202,7 @@ class ConversationTests(unittest.TestCase):
 
     def setUp(self):
         scene_toml = Loader.read_toml(self.scene_toml_text)
+        pprint.pprint(scene_toml)
         assets = Grouping.typewise([Loader.Scene(self.scene_toml_text, scene_toml, None, None, None)])
         world = self.World()
         self.story = StoryBuilder(assets=assets, world=world)
