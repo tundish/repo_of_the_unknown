8e5fddc6d26f35fe6c186f802c240709631efe43
tundish <tundish@gigeconomy.org.uk>
2023-11-16 13:41:16 +0000

    Switch to Peli-Kiera.

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -1,77 +1,48 @@
 # ~/py3.11-blog/bin/pelican -t pelican-stirring content/
 
-AUTHOR = "JunkDLC"
-SITENAME = "Incremental Authorship"
-SITEURL = ""
 
-PATH = "content"
 
-TIMEZONE = "Europe/London"
 
 DEFAULT_LANG = "en"
 
 # SEE
 # https://github.com/hansliu/pelican-stirring#settings
+# https://github.com/aleylara/Peli-Kiera
+THEME = "Peli-Kiera"
+#THEME = "pelican-stirring"
+
+AUTHOR = "JunkDLC"
+SITENAME = "Incremental Authorship"
+SITEURL = ""
+COPYRIGHT = "2019"
+SITESUBTITLE = "Site Subtitle"
+PATH = "content"
+TIMEZONE = "Europe/London"
+DEFAULT_LANG = "en"
+
+#PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ["readtime", "neighbors"]
+STATIC_PATHS = ["images"]
+# Article summary length on main index page
+SUMMARY_MAX_LENGTH = 100
+DEFAULT_PAGINATION = 10
+GITHUB_URL = "https://github.com/"
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+RSS_FEED_SUMMARY_ONLY = True
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("tumblr", "https://tumblr.com/"),
+    ("twitter", "https://twitter.com/"),
+    ("linkedin", "https://www.linkedin.com"),
+    ("github", "https://github.com/"),
+    ("gitlab", "https://gitlab.com/"),
+    ("facebook", "https://facebook.com"),
+    ("instagram", "https://instagram.com"),
 )
-
-DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
-
-DEFAULT_METADATA = {
-"status": "draft",
-}
-
-GOOGLE_CUSTOM_SEARCH = 'your google custom search Id'
-SITEAUTHORS = {
-    'the author name': {
-        'image': 'the author image',
-        'description': 'the author profile'
-    }
-}
-SITECATEGORIES = {
-    'the category name': {
-        'image': 'the category image',
-    }
-}
-SITEFAVICON = 'your favicon.ico path'
-SITECOVER = 'your background cover image path'
-# Replace MENUITEMS, the tuple list support font awesome icon
-THEME_MENUITEMS = (
-    ('Home', '#', 'fas fa-home'),
-    ('You can modify those links in your config file', '#', 'font awesome icon')
-)
-# Replace SOCIAL, the tuple list support font awesome icon
-THEME_SOCIAL = (
-    ('Github', 'https://github.com/hansliu', 'fab fa-github-square fa-2x'),
-    ('You can modify those links in your config file', '#', 'font awesome icon')
-)
-# The INTERNAL LINKS open the linked in current window
-INTERNAL_LINKS = (
-    ('Terms', 'pages/tos.html'),
-    ('Privacy', 'pages/privacy.html'),
-    ('You can modify those links in your config file', '#')
-)
-# Like DISPLAY_PAGES_ON_MENU, you could control DISPLAY_FEEDS_ON_MENU by yourself
-DISPLAY_FEEDS_ON_MENU = True
