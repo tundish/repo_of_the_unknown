3a2843755731aa845fed70dc6d7b58d9550f0f03
tundish <tundish@gigeconomy.org.uk>
2024-01-06 13:19:20 +0000

    Fix path to pelican config.

--- blog/developconf.py
+++ blog/developconf.py
@@ -1,10 +1,11 @@
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-import os
+import pathlib
 import sys
 
-sys.path.append(os.curdir)
+sys.path.append(str(pathlib.Path(__file__).parent))
+
 from pelicanconf import *
 
 RELATIVE_URLS = True
