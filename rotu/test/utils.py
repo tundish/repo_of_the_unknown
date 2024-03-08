8491040d30a4122f0114f16f41e62f85f84f301c
tundish <tundish@gigeconomy.org.uk>
2024-03-08 12:15:59 +0000

    Start to make DB connections.

--- rotu/test/utils.py
+++ rotu/test/utils.py
@@ -18,7 +18,12 @@
 
 import asyncio
 import contextlib
+import os
+import pathlib
 import sqlite3
+import sys
+import tempfile
+import textwrap
 from types import SimpleNamespace
 import unittest
 
@@ -29,6 +34,7 @@ import hypercorn
 from hypercorn.asyncio import serve
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
+from starlette.responses import JSONResponse
 from starlette.responses import PlainTextResponse
 from starlette.routing import Route
 
@@ -36,6 +42,32 @@ import rotu
 
 # Example lifespan-capable ASGI app. Any ASGI app that supports
 # the lifespan protocol will do, e.g. FastAPI, Quart, Responder, ...
+
+sql = SimpleNamespace(
+    create=textwrap.dedent("""
+        CREATE TABLE {db}.witness (
+        name TEXT NOT NULL
+        )
+        """),
+    insert="INSERT INTO {db}.witness ( name ) VALUES ( ? )",
+    select="SELECT ( name ) FROM {db}.witness",
+)
+
+class DBAdaptor:
+
+    @staticmethod
+    def dsn(parent: pathlib.Path, database: str = "main", suffix: str = ".db"):
+        return parent.joinpath(database).with_suffix(suffix).expanduser().as_uri()
+
+    def get_connector(dsn, loop=None):
+        return aiosqlite.connect(
+            dsn,
+            detect_types=sqlite3.PARSE_COLNAMES | sqlite3.PARSE_DECLTYPES,
+            uri=True,
+            loop=loop
+        )
+
+# TODO: ATTACH DATABASE '{db_file_path!s}' AS {db_file_path.name}
 
 @contextlib.asynccontextmanager
 async def lifespan(app):
@@ -48,17 +80,30 @@ class Root(HTTPEndpoint):
         text = f"RotU {rotu.__version__}\n"
         return PlainTextResponse(text)
 
+    async def post(self, request):
+        body = await request.json()
+        try:
+            name = body["name"]
+            print(f"{request.app.state.connector=}", file=sys.stderr)
+            async with request.app.state.connector as connection:
+                await connection.execute(sql.insert.format(db="main"), name)
+            return JSONResponse({"name": name})
+
+        except KeyError:
+            return HTTPException(404, detail="Parameter not recognised", headers=None)
+
 routes = [
-    Route("/", Root, name="root"),
+    Route("/", Root, name="info"),
+    Route("/", Root, methods=["POST"], name="register"),
 ]
-app = Starlette(routes=routes)
+
+def build_app():
+    return Starlette(routes=routes)
 #app = Starlette(lifespan=lifespan)
 
 async def main():
     async with asgi_lifespan.LifespanManager(app) as manager:
         print("We're in!")
-
-events = []
 
 class AsyncConnection:
 
@@ -79,36 +124,72 @@ class AsyncConnection:
 class LifecycleTests(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
-        events.append("setUp")
+        print("setUp", file=sys.stderr)
+        self.temp_fd, temp_db = tempfile.mkstemp(suffix="_tmp.db", dir=pathlib.Path.cwd())
+        self.temp_db = pathlib.Path(temp_db)
+        self.dsn = DBAdaptor.dsn(self.temp_db.parent, self.temp_db.name, self.temp_db.suffix)
+        self.db_connector = DBAdaptor.get_connector(self.dsn)
+
+    async def create_db_fixture(self, connection):
+        await connection.execute(sql.create.format(db="main"))
+        cursor = await connection.execute(sql.select.format(db="main"))
+        rows = await cursor.fetchall()
+        return rows
 
     async def asyncSetUp(self):
         self._async_connection = AsyncConnection()
-        events.append("asyncSetUp")
+        print("asyncSetUp", file=sys.stderr)
 
-    async def test_response(self):
-        events.append("test_response")
+    async def test_endpoint_get(self):
+        print(self.id(), file=sys.stderr)
+
+        async with self.db_connector as connection:
+            rows = await self.create_db_fixture(connection)
+
+        app = build_app()
+        app.state.connector = self.db_connector
 
         transport = httpx.ASGITransport(app=app)
         async with asgi_lifespan.LifespanManager(app) as manager:
             async with httpx.AsyncClient(base_url="http://localhost", transport=transport) as client:
                 response = await client.get("/")
-                print(response.text)
 
         # response = await self._async_connection.get("https://localhost")
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.text.startswith("RotU"), response.text)
+
+    async def test_endpoint_post(self):
+        print(self.id(), file=sys.stderr)
+
+        async with self.db_connector as connection:
+            rows = await self.create_db_fixture(connection)
+
+        print(f"rows=", file=sys.stderr)
+
+        app = build_app()
+        app.state.connector = self.db_connector
+
+        transport = httpx.ASGITransport(app=app)
+        async with asgi_lifespan.LifespanManager(app) as manager:
+            async with httpx.AsyncClient(base_url="http://localhost", transport=transport) as client:
+                response = await client.post("/", json={"name": self.id()})
+
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertTrue(data.get("name", "").endswith("test_endpoint_post") , data)
         self.addAsyncCleanup(self.on_cleanup)
 
     def tearDown(self):
-        events.append("tearDown")
+        print("tearDown", file=sys.stderr)
+        os.close(self.temp_fd)
+        self.temp_db.unlink()
 
     async def asyncTearDown(self):
         await self._async_connection.close()
-        events.append("asyncTearDown")
+        print("asyncTearDown", file=sys.stderr)
 
     async def on_cleanup(self):
-        events.append("cleanup")
-        print(*events, sep="\n")
+        print("cleanup", file=sys.stderr)
 
 
 if __name__ == "__main__":
