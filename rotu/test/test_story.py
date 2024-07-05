a53ceac30c885d9a79213d5f43b517303c148627
tundish <tundish@gigeconomy.org.uk>
2024-07-05 07:46:06 +0100

    Add test module for story.

---
+++ rotu/test/test_story.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+# Copyright 2024 D E Haynes
+
+# This file is part of rotu.
+#
+# Rotu is free software: you can redistribute it and/or modify it under the terms of the
+# GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Rotu is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Rotu.
+# If not, see <https://www.gnu.org/licenses/>.
+
+import unittest
+
+from rotu.main import factory
+
+
+class StoryTests(unittest.TestCase):
+
+    def setUp(self):
+        self.story = factory()
+
+    def test_focus(self):
+        print(f"{self.story=}")
+        print(f"{self.story.world=}")
+        print(f"{self.story.world.map=}")
+        print(f"{self.story.context=}")
