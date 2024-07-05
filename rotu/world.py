7428f42463ebf323758f866dcc33bd990693d5b3
tundish <tundish@gigeconomy.org.uk>
2024-07-05 18:45:32 +0100

    Default types for Transits.

--- rotu/world.py
+++ rotu/world.py
@@ -25,17 +25,18 @@ class Map(MapBuilder):
 
     def build(self):
         yield from [
-            Transit().set_state(self.exit.cafe_f_ext, self.into.car_park, Traffic.flowing),
-            Transit().set_state(self.exit.car_park, self.into.shed_f_ext, Traffic.flowing),
-            Transit().set_state(self.exit.shed_f_ext, self.into.shed_f_int, Traffic.flowing),
-            Transit().set_state(self.exit.shed_f_int, self.into.shed_b_int, Traffic.flowing),
-            Transit().set_state(self.exit.car_park, self.into.van_f_ext, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.cafe_f_ext, self.into.car_park, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.car_park, self.into.shed_f_ext, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.shed_f_ext, self.into.shed_f_int, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.shed_f_int, self.into.shed_b_int, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.car_park, self.into.van_f_ext, Traffic.flowing),
+            Transit(type="View").set_state(self.exit.van_f_int, self.into.van_b_int, Traffic.blocked),
             Transit(
                 names=["Door", "Van door"], type="Door", aspect="unlocked", sketch="The {0.name} is {aspect}"
             ).set_state(self.exit.van_f_ext, self.into.van_f_int, Traffic.flowing),
-            Transit().set_state(self.exit.car_park, self.into.van_b_ext, Traffic.flowing),
-            Transit().set_state(self.exit.van_b_ext, self.into.van_b_int, Traffic.flowing),
-            Transit().set_state(self.exit.van_b_ext, self.into.roadside, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.car_park, self.into.van_b_ext, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.van_b_ext, self.into.van_b_int, Traffic.flowing),
+            Transit(type="Void").set_state(self.exit.van_b_ext, self.into.roadside, Traffic.flowing),
         ]
 
 
